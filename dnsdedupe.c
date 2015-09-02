a264d1724f9e3230f4578c829528e89eae0913fb
Henry Stern <stern@fsi.io>
2015-09-02 11:43:32 -0300

    dnsdedupe: implement formatters and parsers

--- dnsdedupe.c
+++ dnsdedupe.c
@@ -187,7 +187,23 @@ time_format(nmsg_message_t m,
 	    void *ptr,
 	    struct nmsg_strbuf *sb,
 	    const char *endline) {
-	return (nmsg_res_notimpl);
+	nmsg_res res = nmsg_res_failure;
+	time_t t;
+	struct tm gm;
+
+	t = *((uint32_t *) ptr);
+
+	if (gmtime_r(&t, &gm) != NULL) {
+		res = nmsg_strbuf_append(sb, "%d-%02d-%02d %02d:%02d:%02d",
+					 1900 + gm.tm_year,
+					 1 + gm.tm_mon,
+					 gm.tm_mday,
+					 gm.tm_hour,
+					 gm.tm_min,
+					 gm.tm_sec);
+	}
+
+	return (res);
 }
 
 static nmsg_res
@@ -196,8 +212,22 @@ time_parse(nmsg_message_t m,
 	   const char *value,
 	   void **ptr,
 	   size_t *len,
-	   const char *endline) {
-	return (nmsg_res_notimpl);
+	   const char *endline)
+{
+	time_t * t;
+	struct tm gm;
+
+	if (!strptime(value, "%Y-%m-%d %T", &gm)) {
+		return (nmsg_res_parse_error);
+	}
+
+	t = malloc(sizeof(*t));
+	*t = timegm(&gm);
+
+	*ptr = t;
+	*len = sizeof(*t);
+
+	return (nmsg_res_success);
 }
 
 static nmsg_res
@@ -227,8 +257,20 @@ dns_name_format(nmsg_message_t m,
 	        struct nmsg_msgmod_field *field,
 	        void *ptr,
 	        struct nmsg_strbuf *sb,
-	        const char *endline) {
-	return (nmsg_res_notimpl);
+	        const char *endline)
+{
+	ProtobufCBinaryData *rrname = ptr;
+	char name[WDNS_PRESLEN_NAME];
+	nmsg_res res = nmsg_res_success;
+
+	if (rrname->data != NULL &&
+	    rrname->len > 0 &&
+	    rrname->len <= WDNS_MAXLEN_NAME)
+	{
+		wdns_domain_to_str(rrname->data, rrname->len, name);
+		res = nmsg_strbuf_append(sb, "%s", name);
+	}
+	return (res);
 }
 
 static nmsg_res
@@ -238,7 +280,29 @@ dns_name_parse(nmsg_message_t m,
 	       void **ptr,
 	       size_t *len,
 	       const char *endline) {
-	return (nmsg_res_notimpl);
+
+	wdns_res res;
+	wdns_name_t *name;
+
+	name = malloc(sizeof(*name));
+	if (name == NULL) {
+		return (nmsg_res_memfail);
+	}
+
+	res = wdns_str_to_name(value, name);
+	if (res != wdns_res_success) {
+		free (name);
+		return (nmsg_res_parse_error);
+	}
+
+	wdns_downcase_name(name);
+
+	*ptr = name->data;
+	*len = name->len;
+
+	free(name);
+
+	return (nmsg_res_success);
 }
 
 static nmsg_res
@@ -267,17 +331,40 @@ dns_type_format(nmsg_message_t m,
 	        void *ptr,
 	        struct nmsg_strbuf *sb,
 	        const char *endline) {
-	return (nmsg_res_notimpl);
+	uint16_t rrtype;
+	const char *s;
+	nmsg_res res = nmsg_res_success;
+
+	memcpy(&rrtype, ptr, sizeof(rrtype));
+	s = wdns_rrtype_to_str(rrtype);
+	res = nmsg_strbuf_append(sb, "%s", s ? s : "<UNKNOWN>");
+	return (res);
 }
 
 static nmsg_res
-dns_type_parse(nmsg_message_t m,
+dns_type_parse(nmsg_message_t msg,
 	       struct nmsg_msgmod_field *field,
 	       const char *value,
 	       void **ptr,
 	       size_t *len,
 	       const char *endline) {
-	return (nmsg_res_notimpl);
+	uint16_t *rrtype;
+
+	rrtype = malloc(sizeof(*rrtype));
+	if (rrtype == NULL) {
+		return (nmsg_res_memfail);
+	}
+
+	*rrtype = wdns_str_to_rrtype(value);
+	if (*rrtype == 0) {
+		free(rrtype);
+		return (nmsg_res_parse_error);
+	}
+
+	*ptr = rrtype;
+	*len = sizeof(*rrtype);
+
+	return (nmsg_res_success);
 }
 
 static nmsg_res
@@ -306,7 +393,14 @@ dns_class_format(nmsg_message_t m,
 	         void *ptr,
 	         struct nmsg_strbuf *sb,
 	         const char *endline) {
-	return (nmsg_res_notimpl);
+	uint16_t rrclass;
+	const char *s;
+	nmsg_res res = nmsg_res_success;
+
+	memcpy(&rrclass, ptr, sizeof(rrclass));
+	s = wdns_rrclass_to_str(rrclass);
+	res = nmsg_strbuf_append(sb, "%s", s ? s : "<UNKNOWN>");
+	return (res);
 }
 
 static nmsg_res
@@ -316,7 +410,24 @@ dns_class_parse(nmsg_message_t m,
 	        void **ptr,
 	        size_t *len,
 	        const char *endline) {
-	return (nmsg_res_notimpl);
+	uint16_t *rrclass;
+
+	rrclass = malloc(sizeof(*rrclass));
+	if (rrclass == NULL) {
+		return (nmsg_res_memfail);
+	}
+
+	*rrclass = wdns_str_to_rrclass(value);
+	*rrclass = WDNS_CLASS_IN;
+	if (*rrclass == 0) {
+		free(rrclass);
+		return (nmsg_res_parse_error);
+	}
+
+	*ptr = rrclass;
+	*len = sizeof(*rrclass);
+
+	return (nmsg_res_success);
 }
 
 static nmsg_res
@@ -358,12 +469,40 @@ dns_rdata_print(nmsg_message_t msg,
 }
 
 static nmsg_res
-dns_rdata_format(nmsg_message_t m,
+dns_rdata_format(nmsg_message_t msg,
 	         struct nmsg_msgmod_field *field,
 	         void *ptr,
 	         struct nmsg_strbuf *sb,
 	         const char *endline) {
-	return (nmsg_res_notimpl);
+	ProtobufCBinaryData *rdata = ptr;
+	nmsg_res res;
+	char *buf;
+	uint32_t *rrtype, *rrclass;
+	size_t len;
+
+	res = nmsg_message_get_field(msg, "rrtype", 0, (void**) &rrtype, &len);
+	if (res != nmsg_res_success) {
+		return (nmsg_res_failure);
+	}
+	if (len != sizeof(uint32_t)) {
+		return (nmsg_res_failure);
+	}
+
+	res = nmsg_message_get_field(msg, "rrclass", 0, (void**) &rrclass, &len);
+	if (res != nmsg_res_success) {
+		return (nmsg_res_failure);
+	}
+	if (len != sizeof(uint32_t)) {
+		return (nmsg_res_failure);
+	}
+
+	buf = wdns_rdata_to_str(rdata->data, rdata->len, *rrtype, *rrclass);
+	if (buf == NULL)
+		return (nmsg_res_memfail);
+
+	res = nmsg_strbuf_append(sb, "%s", buf);
+	free(buf);
+	return (res);
 }
 
 static nmsg_res
@@ -373,7 +512,35 @@ dns_rdata_parse(nmsg_message_t m,
 	        void **ptr,
 	        size_t *len,
 	        const char *endline) {
-	return (nmsg_res_notimpl);
+	nmsg_res res;
+	wdns_res w_res;
+	uint32_t *rrtype, *rrclass;
+	size_t f_len;
+
+	res = nmsg_message_get_field(m, "rrtype", 0, (void**) &rrtype, &f_len);
+	if (res != nmsg_res_success) {
+		return (nmsg_res_failure);
+	}
+	if (f_len != sizeof(uint32_t)) {
+		return (nmsg_res_failure);
+	}
+
+	res = nmsg_message_get_field(m, "rrclass", 0, (void**) &rrclass, &f_len);
+	if (res != nmsg_res_success) {
+		return (nmsg_res_failure);
+	}
+	if (f_len != sizeof(uint32_t)) {
+		return (nmsg_res_failure);
+	}
+
+	w_res = wdns_str_to_rdata(value, *rrtype, *rrclass, (uint8_t**)ptr, len);
+	if (w_res == wdns_res_parse_error) {
+		return (nmsg_res_parse_error);
+	} else if (w_res != wdns_res_success) {
+		return (nmsg_res_failure);
+	}
+
+	return (nmsg_res_success);
 }
 
 static nmsg_res
@@ -412,12 +579,36 @@ dns_message_print(nmsg_message_t msg,
 }
 
 static nmsg_res
-dns_message_format(nmsg_message_t m,
+dns_message_format(nmsg_message_t msg,
 	           struct nmsg_msgmod_field *field,
 	           void *ptr,
 	           struct nmsg_strbuf *sb,
 	           const char *endline) {
-	return (nmsg_res_notimpl);
+	nmsg_res res;
+	uint8_t *payload;
+	size_t payload_len;
+
+	res = nmsg_message_get_field(msg, field->name, 0, (void **) &payload, &payload_len);
+	if (res == nmsg_res_success) {
+		wdns_message_t dns;
+		wdns_res wres;
+
+		wres = wdns_parse_message(&dns, payload, payload_len);
+		if (wres == wdns_res_success) {
+			char *s;
+
+			s = wdns_message_to_str(&dns);
+			if (s != NULL) {
+				nmsg_strbuf_append(sb, "%s", s);
+				free(s);
+				wdns_clear_message(&dns);
+				return (nmsg_res_success);
+			}
+			wdns_clear_message(&dns);
+		}
+	}
+	nmsg_strbuf_append(sb, "<PARSE ERROR>");
+	return (nmsg_res_success);
 }
 
 static nmsg_res
